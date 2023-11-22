f245179547ddec2802ff2311be2c00b77f89e808
x93125tp <thomas.prior@student.manchester.ac.uk>
2023-11-22 10:28:49 +0000

    Highscores pretty  much done

--- py/fsm/game_solution_state_presentations.py
+++ py/fsm/game_solution_state_presentations.py
@@ -19,26 +19,12 @@ def handle_state_presentation(canvas, data):
     centre = data['width']/2
 
     if data['state'] == 'play':
+
+        draw_play_frame(canvas, data, f_size)
     
-        draw_text(centre, data['height']/3,
-                           "Hit keyboard at {data['target_n']}!!",
-                           f_size)
-
-        draw_text(centre, data['height']/3,
-                           "Hit keyboard at {data['target_n']}!!",
-                           f_size)
-
-        draw_text(centre, data['height']/2,
-                           str(data['random_n']),
-                           f_size)
-
-        draw_text(centre, data['height']/5*4,
-                           f"Level: {data['level']}",
-                           f_size)
-
     elif data['state'] == 'pause':
 
-        draw_text(centre, data['height']/2,
+        draw_text_frame(centre, data['height']/2,
                            "PAUSED!",
                            f_size)
         
@@ -48,19 +34,7 @@ def handle_state_presentation(canvas, data):
 
     elif data['state'] == 'level_up':
 
-        canvas.create_image(
-            centre,
-            data['height'],
-            image=data['frames'][9 - data['play_animation']],
-            anchor='s')
-
-        draw_text(centre, data['height']/2,
-                  "w00t",
-                  f_size)
-
-        if data['play_animation'] == 9:
-
-            subprocess.Popen(["aplay", "mechanical_explosion.wav"])
+        draw_levelup_frame(canvas, data, f_size)
 
     elif data['state'] == 'highscores_input':
 
@@ -74,13 +48,73 @@ def handle_state_presentation(canvas, data):
 
     elif data['state'] == 'highscores_display':
 
-        draw_text(centre, data['height']/3,
-                           "HIGH SCORES TOP 5",
-                           f_size)
+        draw_highscores_display_frame(canvas,data, f_size)
 
-        focus = data['height']/2
 
-        for k, v in data['highscores']:
+def draw_play_frame(canvas, data, f_size):
 
-            draw_text(centre, data
+    draw_text(centre, data['height']/3,
+              "Hit keyboard at {data['target_n']}!!",
+              f_size)
+
+    draw_text(centre, data['height']/3,
+              "Hit keyboard at {data['target_n']}!!",
+              f_size)
+
+    draw_text(centre, data['height']/2,
+              str(data['random_n']),
+              f_size)
+
+    draw_text(centre, data['height']/5*4,
+              f"Level: {data['level']}",
+              f_size)
+
+                      
+def draw_levelup_frame(canvas, data, f_size):
+
+    canvas.create_image(
+        centre,
+        data['height'],
+        image=data['frames'][9 - data['play_animation']],
+        anchor='s')
+
+    draw_text(centre, data['height']/2,
+              "w00t",
+              f_size)
+
+    if data['play_animation'] == 9:
+
+        subprocess.Popen(["aplay", "mechanical_explosion.wav"])
+                      
+
+def draw_highscores_display_frame(canvas, data, f_size):
+
+    draw_text(centre, data['height']/3,
+                       "HIGH SCORES TOP 5",
+                       f_size)
+
+    focus = data['height']/2
+
+    dict_as_tuple = data['highscores'].items()
+    
+    ##   sorted(..., key=lambda item: item[x])
+    ##                ^ Selects the element (by position) to sort by.
+    
+    sorted_scores = sorted(dict_as_tuple, key=lambda item: item[1])
+
+    sorted_scores.reverse()
+
+    pos_y_append = 50
+
+    for score in sorted_scores:
+
+        draw_text(centre, data['height']/3 + pos_y_append,
+                  f"{score[0]} {score[1]}",
+                  f_size)
+
+        pos_y_append += 50
+        
+
+
+
     

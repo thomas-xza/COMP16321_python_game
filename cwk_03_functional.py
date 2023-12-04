dd3204d3454f14db4c1630531e2eb0eb8301880f
x93125tp <thomas.prior@student.manchester.ac.uk>
2023-12-04 16:14:25 +0000

    Still building data struct, but decided to just kind of go with original format, but more manageable, rather than rebuild it for each section.

--- cwk_03_functional.py
+++ cwk_03_functional.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 
 
+#####  SECTION 1
+
+
 def read_file(filename="input.txt"):
 
     file_as_array = []
@@ -14,11 +17,12 @@ def read_file(filename="input.txt"):
     return file_as_array
 
 
+#####  SECTION 2
 
 
 def specific_race_results(data_arr, boat_n, race_x):
 
-    races_data = build_data_structures_section_2(data_arr)
+    races_data = build_data_structure(data_arr)
 
     # print(races_data)
 
@@ -33,34 +37,47 @@ def specific_race_results(data_arr, boat_n, race_x):
     return found_data
 
 
-def build_data_structures_section_2(arr):
+def build_data_structure(arr):
 
-    ##  ['0902-0701-0302-0403-08xx-0605-1006-0207-0508-0909-0910',
-    ##   '0903-0801-0702-0603-05xx-0405-0306-0207-0108-1009-0910']
-    ##  => { 9:
-    ##          { 2: '0701-0302-0403-08xx-0605-1006-0207-0508-0909-0910'
-    ##            3: '0801-0702-0603-05xx-0405-0306-0207-0108-1009-0910' }
+    ##  ['0902-0701-0302-0403-08xx-0605-1006-0207-0508-0909-0110',
+    ##   '0901-0801-0702-0603-05xx-0405-0306-0207-0108-1009-0910']
+    ##  => {
+    ##    data_struct[race_n] = {
+    ##       1: {
+    ##          "boat": 9,
+    ##          "points": 1,
+    ##          "results_raw": '0701-0302-0403-08xx-0605-1006-0207-0508-0909-0910',
+    ##          "results_valid": [7,3,4,6,10,2,5,9,1],
+    ##          "results_invalid": [8],
+    ##          },
+    ##       2: {
+    ##          "boat": 9,
+    ##          "points": 1,
+    ##          "results_raw": '0801-0702-0603-05xx-0405-0306-0207-0108-1009-0910',
+    ##          "results_valid": [8,7,6,4,3,2,1,10,9],
+    ##          "results_invalid": [5],
+    ##          }
     ##     }
 
     data_struct = {}
 
-    race_results_dict = {}
+    race_n = 0
 
     for line in arr:
 
-        boat = extract_data('boat', line)
+        race_n += 1
 
-        race = extract_data('race', line)
+        results_raw =  extract_data('results_raw', line)
+        
+        data_struct[race_n] = {
+            "boat": extract_data('boat', line),
+            "points": extract_data('points', line),
+            "results_raw": results_raw,
+            "results_valid": process_data('results_valid', results_raw),
+            "results_invalid": process_data('results_invalid', results_raw)
+        }
 
-        results = extract_data('results', line)
-
-        race_results_dict = { race: results }
-
-        # print(boat, race, results)
-
-        data_struct[boat] = { **data_struct[boat], race_results_dict }
-
-    # print(data_struct)
+    print(data_struct)
 
     return data_struct
 
@@ -71,24 +88,41 @@ def extract_data(data_type, line):
 
         return int(line.split()[0][0:2])
 
-    elif data_type == 'race':
+    elif data_type == 'points':
 
         return int(line.split()[0][2:4])
 
-    elif data_type == 'results':
+    elif data_type == 'results_raw':
 
         results = line.split('-')
 
         results_2 = results[1:len(results)]
 
-        results_3 = []
-
-        for result in results_2:
-
-            country = result[0:2]
-
-            results_3.append(country)
-
         return results_2
 
         
+def process_data(process_type, results_raw):
+
+    results_arr = results_raw.split('-')
+
+    results_valid = []
+
+    results_invalid = []
+
+    for result in results_arr:
+
+        if 'xx' in result:
+
+            results_invalid.append(result)
+
+        else:
+
+            results_invalid.append(result)
+
+    if process_type == 'results_valid':
+
+        return results_valid
+
+    else:
+
+        return results_invalid
--- test_section_2.py
+++ test_section_2.py
@@ -4,26 +4,57 @@ from cwk_03_functional import *
 
 class Test_section_2(unittest.TestCase):
 
-    def test_section_2_valid(self):
+    def test_build_data_structures_section_2(self):
 
-        file_as_arr = read_file()
+         data_arr = [
+             '0902-0701-0302-0403-08xx-0605-1006-0207-0508-0909-0910',
+             '0901-0801-0702-0603-05xx-0405-0306-0207-0108-1009-0910'
+         ]
 
-        results = specific_race_results(file_as_arr, 5, 2)
+         data_struct = build_data_structures_section_2(data_arr)
 
-        expected_results = '0101-0602-0703-0804-1005-0506-0907-0408-0209-0310'
+         print(data_struct)
 
-        print(results)
+         expected_data_struct = {
+             1: {
+                 "boat": 9,
+                 "points": 1,
+                 "results_raw": '0701-0302-0403-08xx-0605-1006-0207-0508-0909-0910',
+                 "results_valid": [7,3,4,6,10,2,5,9,1],
+                 "results_invalid": [8],
+             },
+             2: {
+                 "boat": 9,
+                 "points": 1,
+                 "results_raw": '0801-0702-0603-05xx-0405-0306-0207-0108-1009-0910',
+                 "results_valid": [8,7,6,4,3,2,1,10,9],
+                 "results_invalid": [5],
+             }
+         }
+         
+         self.assertEqual(data_struct, expected_data_struct)
+
+    # def test_section_2_valid(self):
+
+    #     file_as_arr = read_file()
+
+    #     results = specific_race_results(file_as_arr, 5, 2)
+
+    #     expected_results = '0101-0602-0703-0804-1005-0506-0907-0408-0209-0310'
+
+    #     print(results)
                 
-        self.assertEqual(results, expected_results)
+    #     self.assertEqual(results, expected_results)
 
 
-    def test_section_2_valid_2(self):
+    # def test_section_2_valid_2(self):
 
-        file_as_arr = read_file()
+    #     file_as_arr = read_file()
+        
 
-    def test_section_2_extreme(self):
+    # def test_section_2_extreme(self):
 
-        file_as_arr = read_file()
+    #     file_as_arr = read_file()
 
         
 

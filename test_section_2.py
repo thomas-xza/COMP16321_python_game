8277f39dad2893f654a9901de5446ca062d761b9
x93125tp <thomas.prior@student.manchester.ac.uk>
2023-12-04 16:31:15 +0000

    Wrote another test for process_data()

--- test_section_2.py
+++ test_section_2.py
@@ -35,19 +35,19 @@ class Test_section_2(unittest.TestCase):
     #      self.assertEqual(data_struct, expected_data_struct)
 
 
-    def test_process_data_1(self):
+    def test_process_data_valid_1(self):
 
         results = process_data(
             'results_valid',
-            '0701-0302-0403-08xx-0605-1006-0207-0508-0909-0110'
+            '0701-0302-0403-0808-0605-1006-0207-0508-0909-0110'
         )
 
-        expected_results = [7,3,4,6,10,2,5,9,1]
+        expected_results = [7,3,4,8,6,10,2,5,9,1]
 
         self.assertEqual(results, expected_results)
 
 
-    def test_process_data_2(self):
+    def test_process_data_valid_2(self):
 
         results = process_data(
             'results_invalid',
@@ -57,7 +57,26 @@ class Test_section_2(unittest.TestCase):
         expected_results = [5]
 
         self.assertEqual(results, expected_results)
+
         
+    def test_process_data_extreme_1(self):
+
+        results_v = process_data(
+            'results_invalid',
+            '08xx-07xx-06xx-05xx-04xx-03xx-02xx-01xx-10xx-09xx'
+        )
+
+        results_inv = process_data(
+            'results_valid',
+            '08xx-07xx-06xx-05xx-04xx-03xx-02xx-01xx-10xx-09xx'
+        )
+
+        expected_results = [8,7,6,5,4,3,2,1,10,9]
+
+        self.assertEqual(results_v, expected_results)
+        self.assertEqual(results_inv, [])
+        
+
 
     # def test_section_2_extreme(self):
 

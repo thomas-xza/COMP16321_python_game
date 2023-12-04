ef93d091356a867abd40d8851672ad0c18b82a8c
x93125tp <thomas.prior@student.manchester.ac.uk>
2023-12-04 16:35:44 +0000

    About to try testing the build_data_structure() function

--- test_section_2.py
+++ test_section_2.py
@@ -4,35 +4,35 @@ from cwk_03_functional import *
 
 class Test_section_2(unittest.TestCase):
 
-    # def test_build_data_structures_section_2(self):
+    def test_build_data_structures_section_2(self):
 
-    #      data_arr = [
-    #          '0902-0701-0302-0403-08xx-0605-1006-0207-0508-0909-0910',
-    #          '0901-0801-0702-0603-05xx-0405-0306-0207-0108-1009-0910'
-    #      ]
+         data_arr = [
+             '0902-0701-0302-0403-08xx-0605-1006-0207-0508-0909-0910',
+             '0901-0801-0702-0603-05xx-0405-0306-0207-0108-1009-0910'
+         ]
 
-    #      data_struct = build_data_structure(data_arr)
+         data_struct = build_data_structure(data_arr)
 
-    #      print(data_struct)
+         print(data_struct)
 
-    #      expected_data_struct = {
-    #          1: {
-    #              "boat": 9,
-    #              "points": 1,
-    #              "results_raw": '0701-0302-0403-08xx-0605-1006-0207-0508-0909-0910',
-    #              "results_valid": [7,3,4,6,10,2,5,9,1],
-    #              "results_invalid": [8],
-    #          },
-    #          2: {
-    #              "boat": 9,
-    #              "points": 1,
-    #              "results_raw": '0801-0702-0603-05xx-0405-0306-0207-0108-1009-0910',
-    #              "results_valid": [8,7,6,4,3,2,1,10,9],
-    #              "results_invalid": [5],
-    #          }
-    #      }
+         expected_data_struct = {
+             1: {
+                 "boat": 9,
+                 "points": 1,
+                 "results_raw": '0701-0302-0403-08xx-0605-1006-0207-0508-0909-0910',
+                 "results_valid": [7,3,4,6,10,2,5,9,1],
+                 "results_invalid": [8],
+             },
+             2: {
+                 "boat": 9,
+                 "points": 1,
+                 "results_raw": '0801-0702-0603-05xx-0405-0306-0207-0108-1009-0910',
+                 "results_valid": [8,7,6,4,3,2,1,10,9],
+                 "results_invalid": [5],
+             }
+         }
          
-    #      self.assertEqual(data_struct, expected_data_struct)
+         self.assertEqual(data_struct, expected_data_struct)
 
 
     def test_process_data_valid_1(self):
@@ -75,7 +75,6 @@ class Test_section_2(unittest.TestCase):
 
         self.assertEqual(results_v, expected_results)
         self.assertEqual(results_inv, [])
-        
 
 
     # def test_section_2_extreme(self):
